468d50af519ced92448abd810c0b681ef2b3790e
g@dickins.com <g@dickins.com>
2024-02-02 10:50:19 +0000

    Added a simple MIDI thread to get data from the RME ARC.

--- DepConvolver.cpp
+++ DepConvolver.cpp
@@ -16,6 +16,9 @@
 #include <daes67.hpp>
 #include <log.hpp>
 
+#include <RtMidi.h>
+
+
 using namespace DAES67;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -132,11 +135,75 @@ void dep(void)
 	Debug("DEP Thread Finished");
 }
 
+void arc(void)
+{
+  	RtMidiIn  midiin;
+  	RtMidiOut midiout;
+ 
+	Debug("Starting ARC Thread");
+	else std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	while (g_running)
+	{
+		int nPorts = midiin.getPortCount();
+		for ( unsigned int i=0; i<nPorts; i++ ) 
+		{
+			Debug("Detected MIDI Input device %s",midiin.getPortName(i).c_str());
+			if (midiin.getPortName(i).find("RME ARC") == 0)
+			{
+				midiin.openPort(i);
+				midiin.ignoreTypes(true, true, true);
+				Debug("Opened MIDI Input device %s",midiin.getPortName(i).c_str());
+				break;
+			}
+		}
+		nPorts = midiin.getPortCount();
+
+		for ( unsigned int i=0; i<midiout.getPortCount(); i++ ) 
+		{
+			Debug("Detected MIDI Output device %s",midiout.getPortName(i).c_str());
+			if (midiout.getPortName(i).find("RME ARC") == 0)
+			{
+				midiout.openPort(i);
+				Debug("Opened MIDI Output device %s",midiout.getPortName(i).c_str());
+				break;
+			}
+		}
+
+		if (!midiin.isPortOpen())
+		{
+			Debug("No ARC RME Detected - Waiting to scan ports again");
+			while (g_running && (midiin.getPortCount() == nPorts))
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			}
+			continue;
+		}
+
+		while (g_running)
+		{
+			std::vector<unsigned char> message;
+		 	midiin.getMessage(&message);
+			if (message.size()>0)
+			{	
+				printf("Got message   Length %2d   %02x %02x %02x %02x %02x\n",(int)message.size(),message[0],message[1],message[2],message[3],message[4]);
+				midiout.sendMessage(&message);
+			}	
+			else std::this_thread::sleep_for(std::chrono::milliseconds(1));
+			if  (midiin.getPortCount() != nPorts)
+			{
+				Debug("Change in MIDI devices - rescan");
+				break;
+			}
+		}
+
+		midiin.closePort();
+		midiout.closePort();
+	}
+	Debug("Exiting ARC Thread");
+}
 
 int main(int argc, char * argv[])
 {
-	std::string g;
-
 	// PARSE COMMAND LINE OPTIONS
 	bool bClear=false, bReset=false, bDSP=false, bSilent=false, bLogY=false;
 	int  nDSP[]={ 16, 16, 64, 32, 144, 1, 1}, nTestLength=0;
@@ -179,7 +246,8 @@ int main(int argc, char * argv[])
 	{
 		DSP.Chrono_N(0).configure("COUNT OF LATE CALL EVENTS (samples)",0,800);
 		DSP.Chrono_N(1).configure("COUNT OF DSP OUTPUT TIMES (samples)",-400,400);
-	    std::thread *pthread = new std::thread(dep);
+	    std::thread *depthread = new std::thread(dep);
+	    std::thread *arcthread = new std::thread(arc);
 	}
 
 	Filt[0]=1.0F;
